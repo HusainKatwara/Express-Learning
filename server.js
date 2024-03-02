2ebbcc190a4513f5e1331f5dec978c8a4674ca8a
Husain Katwara <husain.katwara@streebo.com>
2024-03-02 22:38:01 +0530

    2nd commit added env file and started listing

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express');
+require('dotenv').config();
+
+// create express app
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World');
+})
